e231f77a357c8558ba6963d3e3e75d2dd295a202
Eric Dugre <EricD@kentico.com>
2018-12-05 10:16:21 -0500

    Fixed global dependencies and added prepare script

--- delivery.js
+++ delivery.js
@@ -1,11 +1,7 @@
 const KenticoCloud = require('kentico-cloud-delivery-typescript-sdk');
-
 const projectId = '2695019d-6404-00c1-fea5-e0f187569329';
-const previewApiKey = '';
 
 module.exports = new KenticoCloud.DeliveryClient({
   projectId: projectId,
-  typeResolvers: [
-
-  ]
+  typeResolvers: []
 });
