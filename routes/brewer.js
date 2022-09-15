7e2ffbcff271dfe6aee731d9d74e42b196c312cd
Daniel Pokorny <@kontent.ai>
2022-09-15 15:04:59 +0200

    minor tweaks

--- routes/brewer.js
+++ routes/brewer.js
@@ -1,11 +1,14 @@
 import brewerHelper from '../helpers/brewer-helper.js';
 import { Router } from 'express';
+import { resolveRichTextItem } from '../resolvers/rich-text-resolver.js'
 
 const { getBrewer } = brewerHelper;
 const router = Router();
 
 router.get('/:lang/brewer/:codename', async (req, res, next) => {
+    let resolvedCodenames = [];
     const response = await getBrewer(req.params.codename, req.params.lang).catch(next);
+    resolveRichTextItem(response.data.items[0], resolvedCodenames);
 
     res.render('brewer', { 'brewer': response.data.items[0] }, (err, html) => {
         if (err) {
--- routes/coffee.js
+++ routes/coffee.js
@@ -1,11 +1,14 @@
 import coffeHelper from '../helpers/coffee-helper.js';
 import { Router } from 'express';
+import { resolveRichTextItem } from '../resolvers/rich-text-resolver.js'
 
 const { getCoffee } = coffeHelper;
 const router = Router();
 
 router.get('/:lang/coffee/:codename', async (req, res, next) => {
+    let resolvedCodenames = [];
     const response = await getCoffee(req.params.codename, req.params.lang).catch(next);
+    resolveRichTextItem(response.data.items[0], resolvedCodenames);
 
     res.render('coffee', { 'coffee': response.data.items[0] }, (err, html) => {
         if (err) {
